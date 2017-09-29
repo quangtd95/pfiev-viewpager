f7647ef3eab561485dfd82b5bfa55658d4c90a6a
quang.td95 <quang.td95@gmail.com>
2017-09-29 09:09:44 +0700

    longoc

---
+++ app/src/main/java/com/quangtd/demoviewpager/PagerAdapter.java
@@ -0,0 +1,38 @@
+package com.quangtd.demoviewpager;
+
+/**
+ * QuangTD on 9/29/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+class PagerAdapter extends FragmentStatePagerAdapter {
+    private int mNumOfTabs;
+
+    PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            case 2:
+                return new ThirdFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
